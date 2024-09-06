1e625ec63f6458e9cdc9a8b009223cb386b4c44f
David Cordón <157501945+davco0720@users.noreply.github.com>
2024-09-06 10:17:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Ejemplo: código para desplazar suavemente a las secciones
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
