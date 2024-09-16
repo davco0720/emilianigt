132b002e3271d5b1747c5b90fd6a9a722652f0e3
David Cordón <josecordon@emilianisomascos.edu.gt>
2024-09-16 11:10:50 -0600

    update
    update

--- js/script.js
+++ js/script.js
@@ -35,6 +35,27 @@ toggleButton.addEventListener('click', function () {
 const myCarouselElement = document.querySelector('#carouselExampleSlidesOnly')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 1000,
+  interval: 3000,
   touch: false
 })
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
